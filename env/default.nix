32386947f5781250bffde600a6a79816ad38f59f
Nick Spinale <nick@nickspinale.com>
2023-11-10 23:27:56 +0000

    env

---
+++ env/default.nix
@@ -0,0 +1,17 @@
+let
+  topLevel = import ../.;
+
+  inherit (topLevel) pkgs;
+
+  env = pkgs.buildEnv {
+    name = "env";
+    paths = with pkgs; [
+      busybox
+      nix
+      cacert
+      bashInteractive
+    ];
+  };
+in {
+  inherit env;
+}
