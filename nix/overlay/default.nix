3a5d26a68eb408f0a4e6b5d2404386974e515c3a
Nick Spinale <nick@nickspinale.com>
2023-11-14 10:44:33 +0000

    x

--- nix/overlay/default.nix
+++ nix/overlay/default.nix
@@ -16,11 +16,15 @@ in {
 
   isabelle = super.isabelle.overrideAttrs (attrs: {
     postPatch = attrs.postPatch + ''
-      substituteInPlace lib/Tools/env \
-        --replace /usr/bin/env ${coreutils}/bin/env
+      substituteInPlace \
+        lib/Tools/env \
+          --replace /usr/bin/env ${coreutils}/bin/env
 
-      substituteInPlace src/Pure/General/sha1.ML \
-        --replace '"$ML_HOME/" ^ (if ML_System.platform_is_windows then "sha1.dll" else "libsha1.so")' '"${this.isabelle-sha1}/lib/libsha1.so"'
+      substituteInPlace \
+        src/Pure/General/sha1.ML \
+          --replace \
+            '"$ML_HOME/" ^ (if ML_System.platform_is_windows then "sha1.dll" else "libsha1.so")' \
+            '"${this.isabelle-sha1}/lib/libsha1.so"'
     '';
   });
 }
