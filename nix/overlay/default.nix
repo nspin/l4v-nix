2ddf5c971f243d2d423aeee9e4f2b3ea59e645e3
Nick Spinale <nick@nickspinale.com>
2023-11-15 10:03:30 +0000

    Refactor overlay

--- nix/overlay/default.nix
+++ nix/overlay/default.nix
@@ -3,44 +3,22 @@ self: super: with self;
 let
   pythonOverrides = callPackage ./python-overrides.nix {};
 
-  mkThis = args: lib.makeScope newScope (callPackage ../scope {} args);
+  mkScope = args: lib.makeScope newScope (callPackage ../scope {} args);
+
+  mkL4vConfig =
+    { arch
+    , optLevel ? "-O1"
+    , targetCC ? targetPkgsByL4vArch."${arch}".stdenv.cc
+    , targetPrefix ? targetCC.targetPrefix
+    }:
+    {
+      inherit arch optLevel targetCC targetPrefix;
+    };
 
   targetPkgsByL4vArch = {
     "ARM" = armv7Pkgs;
     "ARM_HYP" = armv7Pkgs;
     "X64" = x64Pkgs;
-  };
-
-  mkL4vConfig = { arch, optLevel ? "-O1" }:
-    let
-      targetPkgs = targetPkgsByL4vArch."${arch}";
-    in {
-      inherit arch optLevel;
-      targetPrefix = targetPkgs.stdenv.cc.targetPrefix;
-      targetCC = targetPkgs.stdenv.cc;
-    };
-
-in {
-  this = rec {
-    default = arm;
-
-    arm = mkThis {
-      l4vConfig = mkL4vConfig {
-        arch = "ARM";
-      };
-    };
-
-    armHyp = mkThis {
-      l4vConfig = mkL4vConfig {
-        arch = "ARM_HYP";
-      };
-    };
-
-    x86 = mkThis {
-      l4vConfig = mkL4vConfig {
-        arch = "X64";
-      };
-    };
   };
 
   armv7Pkgs = import ../../nixpkgs {
@@ -51,6 +29,31 @@ in {
   };
 
   x64Pkgs = self;
+
+in {
+  this = rec {
+    inherit armv7Pkgs x64Pkgs;
+
+    default = arm;
+
+    arm = mkScope {
+      l4vConfig = mkL4vConfig {
+        arch = "ARM";
+      };
+    };
+
+    armHyp = mkScope {
+      l4vConfig = mkL4vConfig {
+        arch = "ARM_HYP";
+      };
+    };
+
+    x86 = mkScope {
+      l4vConfig = mkL4vConfig {
+        arch = "X64";
+      };
+    };
+  };
 
   python2 = super.python2.override {
     packageOverrides = pythonOverrides;
