018066db572b882de86e67d38aa6cb8749f1b963
Nick Spinale <nick@nickspinale.com>
2023-11-15 02:48:14 +0000

    x

--- nix/overlay/default.nix
+++ nix/overlay/default.nix
@@ -7,6 +7,8 @@ let
 
   targetPkgsByL4vArch = {
     "ARM" = armv7Pkgs;
+    "ARM_HYP" = armv7Pkgs;
+    "X64" = x64Pkgs;
   };
 
   mkL4vConfig = { arch, optLevel ? "-O1" }:
@@ -25,12 +27,26 @@ in {
     };
   };
 
+  thisArmHyp = mkThis {
+    l4vConfig = mkL4vConfig {
+      arch = "ARM_HYP";
+    };
+  };
+
+  thisX64 = mkThis {
+    l4vConfig = mkL4vConfig {
+      arch = "X64";
+    };
+  };
+
   armv7Pkgs = import ../../nixpkgs {
     crossSystem = {
       system = "armv7l-linux";
       config = "armv7l-unknown-linux-gnueabi";
     };
   };
+
+  x64Pkgs = self;
 
   python2 = super.python2.override {
     packageOverrides = pythonOverrides;
