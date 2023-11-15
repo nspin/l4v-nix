3a8f310075022763d46bf24f5c3d64825e2622be
Nick Spinale <nick@nickspinale.com>
2023-11-15 21:28:33 +0000

    x

--- env/default.nix
+++ env/default.nix
@@ -1,11 +1,5 @@
 let
-  nixpkgs = builtins.fetchGit {
-    url = "https://github.com/NixOS/nixpkgs.git";
-    ref = "nixos-unstable";
-    rev = "85f1ba3e51676fa8cc604a3d863d729026a6b8eb";
-  };
-
-  pkgs = import nixpkgs {};
+  pkgs = import ../nixpkgs {};
 
   runInContainer = pkgs.callPackage ./run-in-container.nix {};
 
