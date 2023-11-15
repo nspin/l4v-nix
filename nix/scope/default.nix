4f51b58ee4f6f4e050dd70ebf15378919fe917b0
Nick Spinale <nick@nickspinale.com>
2023-11-15 10:12:31 +0000

    x

--- nix/scope/default.nix
+++ nix/scope/default.nix
@@ -12,7 +12,7 @@ let
   oldNixpkgsSource = builtins.fetchGit {
     url = "https://github.com/NixOS/nixpkgs.git";
     ref = "nixos-unstable";
-    rev = "85f1ba3e51676fa8cc604a3d863d729026a6b8eb";
+    rev = "d4b654cb468790e7ef204ade22aed9b0d9632a7b";
   };
 
   oldPkgs = import oldNixpkgsSource {};
