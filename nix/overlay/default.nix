0be65df955dd96c378e2e62b70906f0670ba56ed
x <x@xs-MacBook-Air.local>
2023-11-25 00:30:07 -0500

    x

--- nix/overlay/default.nix
+++ nix/overlay/default.nix
@@ -7,7 +7,7 @@ let
 
   mkL4vConfig =
     let
-      defaultCCWrapper = arch: targetPkgsByL4vArch."${arch}".buildPackages.gcc8;
+      defaultCCWrapper = arch: targetPkgsByL4vArch."${arch}".buildPackages.gcc48;
     in
     { arch
     , optLevel ? "-O1"
