4ce4db04ac0016302fd6e789dc24bf8866fe4216
Nick Spinale <nick@nickspinale.com>
2023-11-26 21:50:17 +0000

    x

--- nix/scope/wip/default.nix
+++ nix/scope/wip/default.nix
@@ -54,11 +54,13 @@ let
 in {
   a = graphRefineWith {
     solverList = with graphRefineSolverLists; writeText "solverlist" ''
-      CVC4: online: ${wrap} tlogs/online ${cvc4BinaryExe} --incremental --lang smt --tlimit=0
-      SONOLAR: offline: ${wrap} tlogs/offline ${sonolarBinaryExe} --input-format=smtlib2
+      CVC4: online: ${cvc4BinaryExe} --incremental --lang smt --tlimit=0
       # CVC4: offline: ${wrap} tlogs/offline ${cvc4BinaryExe} --lang smt
-      # SONOLAR-word8: offline: ${sonolarBinaryExe} --input-format=smtlib2
+      # CVC4: offline: ${wrap} tlogs/offline ${cvc4BinaryExe} --lang smt
         # config: mem_mode = 8
+      # SONOLAR: offline: ${wrap} tlogs/offline ${sonolarBinaryExe} --input-format=smtlib2
+      SONOLAR-word8: offline: ${wrap} tlogs/offline ${sonolarBinaryExe} --input-format=smtlib2
+        config: mem_mode = 8
     '';
     targetDir = graphRefine.justStackBounds;
     # source = lib.cleanSource ../../tmp/graph-refine;
@@ -85,14 +87,30 @@ in {
     ];
   };
 
+  reproGetSmt = graphRefineWith {
+    solverList = with graphRefineSolverLists; writeText "solverlist" ''
+      CVC4: online: ${cvc4BinaryExe} --incremental --lang smt --tlimit=0
+      # CVC4: offline: ${wrap} tlogs/offline ${cvc4BinaryExe} --lang smt
+      # CVC4: offline: ${wrap} tlogs/offline ${cvc4BinaryExe} --lang smt
+        # config: mem_mode = 8
+      SONOLAR: offline: ${wrap} tlogs/offline ${sonolarBinaryExe} --input-format=smtlib2
+      # SONOLAR-word8: offline: ${wrap} tlogs/offline ${sonolarBinaryExe} --input-format=smtlib2
+        # config: mem_mode = 8
+    '';
+    targetDir = graphRefine.justStackBounds;
+    args = [
+      "verbose" "trace-to:report.txt" "deps:Kernel_C.memcpy"
+    ];
+  };
+
   repro =
     let
       d = ./artifacts/sonolar-bug-repro;
+      solvers = [ "sonolar" "cvc4" ];
       cmd = {
         sonolar = "sonolar --input-format=smtlib2";
         cvc4 = "cvc4 --lang smt";
       };
-      solvers = [ "sonolar" "cvc4" ];
     in runCommand "x" {
       nativeBuildInputs = [
         graphRefineSolverLists.selectedCvc4Binary
