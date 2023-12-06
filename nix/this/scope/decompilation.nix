bbd82ce718fd496bc11b8cdd4eb88ccaabfc3fc7
Nick Spinale <nick@nickspinale.com>
2023-12-06 03:18:48 +0000

    revert decomp

--- nix/this/scope/decompilation.nix
+++ nix/this/scope/decompilation.nix
@@ -8,19 +8,20 @@
 }:
 
 let
-  # ignore = [
-  #   "_start" "c_handle_fastpath_call" "c_handle_fastpath_reply_recv" "restore_user_context"
-  # ];
+  ignore = [
+    "_start" "c_handle_fastpath_call" "c_handle_fastpath_reply_recv" "restore_user_context"
+  ];
 
-  ignore = runCommand "ignore" {} ''
-    cat ${kernel}/kernel.sigs | cut -d ' ' -f 2 | grep -v memzero | tr '\n' ',' | sed 's/,$/\n/' > $out
-  '';
+  # ignore = runCommand "ignore" {} ''
+  #   cat ${kernel}/kernel.sigs | cut -d ' ' -f 2 | grep -v memzero | tr '\n' ',' | sed 's/,$/\n/' > $out
+  # '';
 
   # TODO rename to .sml
   scriptIn = writeText "x.ml" ''
     load "decompileLib";
-    val _ = decompileLib.decomp "@path@" true "@ignore@";
+    val _ = decompileLib.decomp "@path@" true "${lib.concatStringsSep "," ignore}";
   '';
+    # val _ = decompileLib.decomp "@path@" true "@ignore@";
 
 in
 runCommand "decompilation" {
@@ -38,12 +39,14 @@ runCommand "decompilation" {
   mkdir $target_dir
   cp ${kernel}/{kernel.elf.txt,kernel.sigs} target
 
-  substitute ${scriptIn} $script \
-    --subst-var-by path $target_dir/kernel \
-    --subst-var-by ignore $(cat ${ignore})
+  substitute ${scriptIn} $script --subst-var-by path $target_dir/kernel
 
   cd $hol_dir/examples/machine-code/graph
   echo "decompiling..."
   $hol_dir/bin/hol < $script > $target_dir/log.txt
   cp -r $target_dir $out
 ''
+
+  # substitute ${scriptIn} $script \
+  #   --subst-var-by path $target_dir/kernel \
+  #   --subst-var-by ignore $(cat ${ignore})
