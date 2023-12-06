f34e252a900777159c7873c6c24628d7d7b756b3
Nick Spinale <nick@nickspinale.com>
2023-12-06 07:48:30 +0000

    x

--- nix/this/scope/wip/default.nix
+++ nix/this/scope/wip/default.nix
@@ -75,12 +75,23 @@ in rec {
     };
   });
 
+  cs = checkpointScopes;
+
   checkpointScopes = {
     h120 = scopeWithHOL4Rev "6c0c2409ecdbd7195911f674a77bfdd39c83816e";
     h121 = scopeWithHOL4Rev "ab03cec5200c8b23f9ba60c5cea958cfcd0cd158";
     good = scopeWithHOL4Rev "6d809bfa2ef8cbcb75d63317c4f8f2e1a6a836ed";
     bad = scopeWithHOL4Rev "bd30aea4dae85d51001ea398c59d2459a3e57dc6";
     sdcr = scopeWithHOL4Rev "553e7165b4d27ecda9b69913728e93f8f3f7b891";
+    upstream = overrideScope (self: super: {
+      scopeConfig = super.scopeConfig.override {
+        hol4Source = lib.cleanSource (builtins.fetchGit {
+          url = "https://github.com/HOL-Theorem-Prover/HOL.git";
+          rev = self.hol4Rev;
+        });
+      };
+      hol4Rev = "3f6e78258b82149b95ab354e180b95cd094ec4e7";
+    });
   };
 
   checkpoint = linkFarm "checkpoint" (lib.flip lib.mapAttrs checkpointScopes (_: scope:
