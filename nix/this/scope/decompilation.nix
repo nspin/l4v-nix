f45818b0842ed11d32a403e65c48231a38914c15
Nick Spinale <nick@nickspinale.com>
2023-12-06 01:48:25 +0000

    just memzero

--- nix/this/scope/decompilation.nix
+++ nix/this/scope/decompilation.nix
@@ -8,14 +8,18 @@
 }:
 
 let
-  ignore = [
-    "_start" "c_handle_fastpath_call" "c_handle_fastpath_reply_recv" "restore_user_context"
-  ];
+  # ignore = [
+  #   "_start" "c_handle_fastpath_call" "c_handle_fastpath_reply_recv" "restore_user_context"
+  # ];
+
+  ignore = runCommand "ignore" {} ''
+    cat ${kernel}/kernel.sigs | cut -d ' ' -f 2 | tr '\n' ',' | sed 's/,$/\n/' > $out
+  '';
 
   # TODO rename to .sml
   scriptIn = writeText "x.ml" ''
     load "decompileLib";
-    val _ = decompileLib.decomp "@path@" true "${lib.concatStringsSep "," ignore}";
+    val _ = decompileLib.decomp "@path@" true "@ignore@";
   '';
 
 in
@@ -34,7 +38,9 @@ runCommand "decompilation" {
   mkdir $target_dir
   cp ${kernel}/{kernel.elf.txt,kernel.sigs} target
 
-  substitute ${scriptIn} $script --subst-var-by path $target_dir/kernel
+  substitute ${scriptIn} $script \
+    --subst-var-by path $target_dir/kernel \
+    --subst-var-by ignore $(cat ${ignore})
 
   cd $hol_dir/examples/machine-code/graph
   echo "decompiling..."
