fd7afa0989105e96d8c58cada282ba451c7e2bb2
Nick Spinale <nick@nickspinale.com>
2023-12-08 15:56:09 +0000

    x

--- nix/this/scope/wip/default.nix
+++ nix/this/scope/wip/default.nix
@@ -26,81 +26,6 @@ let
 
 in rec {
 
-  seL4_12_0_0 = overrideScope (self: super: {
-    scopeConfig = super.scopeConfig.override {
-      seL4Source = builtins.fetchGit {
-        url = "https://github.com/seL4/seL4";
-        rev = "dc83859f6a220c04f272be17406a8d59de9c8fbf";
-      };
-      l4vSource = builtins.fetchGit {
-        url = "https://github.com/seL4/l4v";
-        rev = "6700d97b7f0593dbf5d8145ee43f1e151553dea0";
-      };
-      hol4Source = lib.cleanSource (builtins.fetchGit {
-        url = "https://github.com/seL4/HOL";
-        rev = "6c0c2409ecdbd7195911f674a77bfdd39c83816e";
-      });
-      isabelleVersion = "2020";
-      stackLTSAttr = "lts_13_15";
-      targetCCWrapperAttr = "gcc49";
-    };
-  });
-
-  scopeWithHOL4Rev = rev: overrideScope (self: super: {
-    scopeConfig = super.scopeConfig.override {
-      hol4Source = lib.cleanSource (builtins.fetchGit {
-        url = "https://github.com/coliasgroup/HOL";
-        inherit rev;
-      });
-    };
-    hol4Rev = rev;
-  });
-
-  evidenceScopes = {
-    at120 = scopeWithHOL4Rev "6c0c2409ecdbd7195911f674a77bfdd39c83816e";
-    at121 = scopeWithHOL4Rev "ab03cec5200c8b23f9ba60c5cea958cfcd0cd158";
-    good = scopeWithHOL4Rev "6d809bfa2ef8cbcb75d63317c4f8f2e1a6a836ed";
-    bad = scopeWithHOL4Rev "bd30aea4dae85d51001ea398c59d2459a3e57dc6";
-    current = scopeWithHOL4Rev "39606aea49bbfef131fcad2af088800e4b048da3";
-  };
-
-  evidence = linkFarm "evidence" (lib.flip lib.mapAttrs evidenceScopes (_: scope:
-    linkFarm "scope" (
-      {
-        "rev" = writeText "rev.txt" scope.hol4Rev;
-        "kernel.elf.txt" = "${scope.kernel}/kernel.elf.txt";
-        "kernel.sigs" = "${scope.kernel}/kernel.sigs";
-        "kernel_mc_graph.txt" = "${scope.decompilation}/kernel_mc_graph.txt";
-        "log.txt" = "${scope.decompilation}/log.txt";
-        "report.txt" = "${scope.wip.justMemzero}/report.txt";
-      }
-    )
-  ));
-
-  keep = writeText "kleep" (toString (lib.flatten [
-    seL4_12_0_0.graphRefine.all
-    allExceptInitFreemem
-    kernels
-  ]));
-
-  kernels = writeText "x" (toString (this.mkAggregate (
-    { archName, targetCCWrapperAttrName, optLevelName }:
-    let
-      scope = this.byConfig.${archName}.${targetCCWrapperAttrName}.${optLevelName};
-    in
-      lib.optionals (lib.all lib.id [
-        (lib.elem scope.scopeConfig.arch [
-          "ARM"
-        ])
-      ])
-      [
-        scope.kernel
-      ]
-  )));
-
-  prime = writeText "prime" (toString (lib.flatten [
-  ]));
-
   allExceptInitFreemem = graphRefineWith {
     args = [
       "trace-to:report.txt"
@@ -128,4 +53,81 @@ in rec {
     ];
   };
 
+  seL4_12_0_0 = overrideScope (self: super: {
+    scopeConfig = super.scopeConfig.override {
+      seL4Source = builtins.fetchGit {
+        url = "https://github.com/seL4/seL4";
+        rev = "dc83859f6a220c04f272be17406a8d59de9c8fbf";
+      };
+      l4vSource = builtins.fetchGit {
+        url = "https://github.com/seL4/l4v";
+        rev = "6700d97b7f0593dbf5d8145ee43f1e151553dea0";
+      };
+      hol4Source = lib.cleanSource (builtins.fetchGit {
+        url = "https://github.com/seL4/HOL";
+        rev = "6c0c2409ecdbd7195911f674a77bfdd39c83816e";
+      });
+      isabelleVersion = "2020";
+      stackLTSAttr = "lts_13_15";
+      targetCCWrapperAttr = "gcc49";
+    };
+  });
+
+  scopeWithHOL4Rev = { rev, ref ? "HEAD" }: overrideScope (self: super: {
+    scopeConfig = super.scopeConfig.override {
+      hol4Source = lib.cleanSource (builtins.fetchGit {
+        url = "https://github.com/coliasgroup/HOL";
+        inherit rev ref;
+      });
+    };
+    hol4Rev = rev;
+  });
+
+  evidenceScopes = {
+    at120 = scopeWithHOL4Rev { rev = "6c0c2409ecdbd7195911f674a77bfdd39c83816e"; };
+    at121 = scopeWithHOL4Rev { rev = "ab03cec5200c8b23f9ba60c5cea958cfcd0cd158"; };
+    good = scopeWithHOL4Rev { rev = "6d809bfa2ef8cbcb75d63317c4f8f2e1a6a836ed"; };
+    bad = scopeWithHOL4Rev { rev = "bd30aea4dae85d51001ea398c59d2459a3e57dc6"; };
+    current = scopeWithHOL4Rev rec {
+      rev = "39606aea49bbfef131fcad2af088800e4b048da3";
+      ref = "refs/tags/keep/${builtins.substring 0 32 rev}";
+    };
+  };
+
+  evidence = linkFarm "evidence" (lib.flip lib.mapAttrs evidenceScopes (_: scope:
+    linkFarm "scope" (
+      {
+        "rev" = writeText "rev.txt" scope.hol4Rev;
+        "kernel.elf.txt" = "${scope.kernel}/kernel.elf.txt";
+        "kernel.sigs" = "${scope.kernel}/kernel.sigs";
+        "kernel_mc_graph.txt" = "${scope.decompilation}/kernel_mc_graph.txt";
+        "log.txt" = "${scope.decompilation}/log.txt";
+        "report.txt" = "${scope.wip.justMemzero}/report.txt";
+      }
+    )
+  ));
+
+  kernels = writeText "x" (toString (this.mkAggregate (
+    { archName, targetCCWrapperAttrName, optLevelName }:
+    let
+      scope = this.byConfig.${archName}.${targetCCWrapperAttrName}.${optLevelName};
+    in
+      lib.optionals (lib.all lib.id [
+        (lib.elem scope.scopeConfig.arch [
+          "ARM"
+        ])
+      ])
+      [
+        scope.kernel
+      ]
+  )));
+
+  keep = writeText "kleep" (toString (lib.flatten [
+    seL4_12_0_0.graphRefine.all
+    allExceptInitFreemem
+    kernels
+  ]));
+
+  prime = writeText "prime" (toString (lib.flatten [
+  ]));
 }
