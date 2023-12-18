2e785b001bfa17330a3f12f923af1570fe8e2f92
Nick Spinale <nick@nickspinale.com>
2023-12-18 05:31:06 +0000

    x

--- nix/this/scope/deps/isabelle-for-l4v/common/default.nix
+++ nix/this/scope/deps/isabelle-for-l4v/common/default.nix
@@ -1,11 +1,17 @@
 { lib
 , fetchurl
-, isabellSource
+, isabelleSource
 }:
+
+let
+  isabelleSource = lib.cleanSource ../../../../../../tmp/src/isabelle;
+in
 
 rec {
   x = fetchurl {
     url = "https://isabelle.in.tum.de/components/cakeml-2.0.tar.gz";
     sha1 = "f92cff635dfba5d4d77f469307369226c868542c";
   };
+
+  y = isabelleSource;
 }
