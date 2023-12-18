e5846b1015ae854bd878a566a92ce8ccb4ed5cd0
Nick Spinale <nick@nickspinale.com>
2023-12-18 13:17:31 +0000

    x

--- nix/this/scope/deps/isabelle-for-l4v/common/default.nix
+++ nix/this/scope/deps/isabelle-for-l4v/common/default.nix
@@ -1,5 +1,8 @@
 { lib
+, stdenv
 , fetchurl
+, autoPatchelfHook
+
 , isabelleSource
 }:
 
@@ -12,6 +15,53 @@ rec {
     url = "https://isabelle.in.tum.de/components/cakeml-2.0.tar.gz";
     sha1 = "f92cff635dfba5d4d77f469307369226c868542c";
   };
+
+  parseHashesFile = contents:
+    let
+      pairs = lib.filter lib.isList (builtins.split "([^ ]*) ([^\n]*)\n" contents);
+      f = pair:
+        let
+          hash = lib.elemAt pair 0;
+          fname = lib.elemAt pair 1;
+          m = builtins.match "(.*)\\.tar\\.gz" fname;
+          name = assert m != null; lib.elemAt m 0;
+        in
+          lib.nameValuePair name hash;
+    in
+      lib.listToAttrs (map f pairs);
+
+  fetchComponent = { name, sha1 }: fetchurl {
+    url = "https://isabelle.in.tum.de/components/${name}.tar.gz";
+    inherit sha1;
+  };
+
+  mkComponent = { name, src }: stdenv.mkDerivation {
+    name = "isabelle-component-${name}";
+    inherit src;
+    nativeBuildInputs = [
+      autoPatchelfHook
+    ];
+    buildInputs = [
+      stdenv.cc.cc.lib
+    ];
+    phases = [ "unpackPhase" "patchPhase" "installPhase" "fixupPhase" ];
+    installPhase = ''
+      cp -r . $out
+    '';
+  };
+
+  z =
+    let
+      path = isabelleSource + "/Admin/components/components.sha1";
+      contents = lib.readFile path;
+      hashes = parseHashesFile contents;
+    in
+      lib.flip lib.mapAttrs hashes (name: sha1: mkComponent {
+        inherit name;
+        src = fetchComponent {
+          inherit name sha1;
+        };
+      });
 
   y = isabelleSource;
 }
