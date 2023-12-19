5d14ebeea43839b3d85e5ed55a53fe90e9da3890
Nick Spinale <nick@nickspinale.com>
2023-12-19 10:01:54 +0000

    x

--- nix/this/scope/deps/isabelle-for-l4v/common/default.nix
+++ nix/this/scope/deps/isabelle-for-l4v/common/default.nix
@@ -1,5 +1,6 @@
 { lib
-, stdenv
+, stdenv, hostPlatform
+, runCommand, writeText
 , fetchurl
 , autoPatchelfHook
 
@@ -8,14 +9,11 @@
 
 let
   isabelleSource = lib.cleanSource ../../../../../../tmp/src/isabelle;
+
+  componentExtension = self: super: {};
 in
 
 rec {
-  x = fetchurl {
-    url = "https://isabelle.in.tum.de/components/cakeml-2.0.tar.gz";
-    sha1 = "f92cff635dfba5d4d77f469307369226c868542c";
-  };
-
   parseHashesFile = contents:
     let
       pairs = lib.filter lib.isList (builtins.split "([^ ]*) ([^\n]*)\n" contents);
@@ -29,6 +27,13 @@ rec {
           lib.nameValuePair name hash;
     in
       lib.listToAttrs (map f pairs);
+
+  parseComponentListFile = contents:
+    let
+      lines = map lib.head (lib.filter lib.isList (builtins.split "([^\n]*)\n" contents));
+      ignore = line: lib.stringLength line == 0 || lib.hasPrefix "#" line;
+    in
+      lib.filter (line: !(ignore line)) lines;
 
   fetchComponent = { name, sha1 }: fetchurl {
     url = "https://isabelle.in.tum.de/components/${name}.tar.gz";
@@ -50,18 +55,52 @@ rec {
     '';
   };
 
-  z =
-    let
-      path = isabelleSource + "/Admin/components/components.sha1";
-      contents = lib.readFile path;
-      hashes = parseHashesFile contents;
-    in
-      lib.flip lib.mapAttrs hashes (name: sha1: mkComponent {
-        inherit name;
-        src = fetchComponent {
-          inherit name sha1;
-        };
-      });
+  mkLocalComponent =
+    { name, settings ? null, options ? null, components ? null, passthru ? {} }:
+    runCommand "isabelle-local-component-${name}" {
+      inherit passthru;
+    } (''
+      mkdir -p $out/etc
+    '' + lib.optionalString (settings != null) ''
+      ln -s ${writeText "settings" settings} $out/etc/settings
+    '' + lib.optionalString (options != null) ''
+      ln -s ${writeText "options" options} $out/etc/options
+    '' + lib.optionalString (components != null) ''
+      ln -s ${writeText "components" (lib.concatMapStrings (x: ''
+        ${x}
+      '') components)} $out/etc/components
+    '');
 
-  y = isabelleSource;
+  metaComponent = src:
+    let
+      componentHashes = parseHashesFile (lib.readFile (src + "/Admin/components/components.sha1"));
+      allComponentsBeforeExtension =
+        lib.flip lib.mapAttrs componentHashes (name: sha1: mkComponent {
+          inherit name;
+          src = fetchComponent {
+            inherit name sha1;
+          };
+        });
+      allComponents = lib.fix (lib.extends componentExtension (lib.const allComponentsBeforeExtension));
+      platformSuffix =
+        assert hostPlatform.isLinux;
+        "linux${lib.optionalString hostPlatform.isAarch64 "_arm"}";
+      componentListFnames = [
+        "main"
+        "bundled"
+        "bundled-${platformSuffix}"
+        "cakeml"
+        "optional"
+        "nonfree"
+      ];
+      componentList = lib.flip lib.concatMap componentListFnames
+        (fname: parseComponentListFile (lib.readFile (src + "/Admin/components/${fname}")));
+      components = map (componentName: allComponents.${componentName}) componentList;
+    in
+      mkLocalComponent {
+        name = "meta";
+        inherit components;
+      };
+
+    x = metaComponent isabelleSource;
 }
