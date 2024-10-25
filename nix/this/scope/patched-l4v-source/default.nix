953647a55f8bf866c9f723036c2e782d5725959a
Nick Spinale <nick@nickspinale.com>
2024-10-25 04:38:13 -0700

    Remove unnecessary SEL4.cabal patch

--- nix/this/scope/patched-l4v-source/default.nix
+++ nix/this/scope/patched-l4v-source/default.nix
@@ -17,8 +17,6 @@ stdenv.mkDerivation {
   postPatch = ''
     patchShebangs .
 
-    sed -i -E 's/^(license: +)GPL$/\1GPL-2.0-only/' spec/haskell/SEL4.cabal
-
     substituteInPlace spec/haskell/Makefile \
       --replace 'sandbox: .stack-work' 'sandbox:' \
       --replace 'CABAL=stack exec -- ./stack-path cabal' 'CABAL=cabal' \
