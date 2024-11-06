1e47c6b75c88be36121fdda990948f483e1b5276
Nick Spinale <nick@nickspinale.com>
2024-11-06 03:37:22 -0800

    docker: Fix elsewhere.mk

--- docker/elsewhere.mk
+++ docker/elsewhere.mk
@@ -1,10 +1,18 @@
-W ?= $(realpath .)
-H ?= $(realpath isabelle-home-user)
-ID ?=
+ID ?= elsewhere
+W ?= $(abspath .)
+H ?= $(abspath isabelle-home-user)
+
+C ?= ARM
 
 docker_dir := $(dir $(lastword $(MAKEFILE_LIST)))
 
 none build run exec rm-container rm-isabelle-user-home clean:
 
 %:
-	$(MAKE) -C $(docker_dir) $@
+	$(MAKE) \
+		-C $(docker_dir) \
+		ID=$(ID) \
+		W=$(W) \
+		H=$(H) \
+		C=$(C) \
+		$@
